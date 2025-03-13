53b9916395252b7b6bf288403907f6ffb2bc7e2b
Elmari121 <robbertselmari@gmail.com>
2025-03-13 13:28:52 +0200

    corrected html and js

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 function updateweatherdata(response) {
-  let tempelement = document.querySelector("#Temperature");
-  let Temperature = response.data.Temperature.current;
-  tempelement.innerHTML = math.round(Temperature);
-  let;
+  let tempelement = document.querySelector("#temp");
+  let Temperature = response.data.main.temp;
+  tempelement.innerHTML = Math.round(Temperature);
+
+  let cityelement = document.querySelector("#city");
+  cityelement.innerHTML = response.data.name;
 }
+
 function searchcity(city) {
   let apikey = "30927dtfa44b4770359oe8258a9c5b2c";
-  let apiurl =
-    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric";
+  let apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
+
   axios.get(apiurl).then(updateweatherdata);
 }
+
 function handlesearchsubmit(event) {
-  event.preventdefault();
+  event.preventDefault();
   let searchinput = document.querySelector("#search-form-input");
   let cityelement = document.querySelector("#city");
   cityelement.innerHTML = searchinput.value;
   searchcity(searchinput.value);
 }
 
-let searchformelement = document.querySelector("search-form");
+let searchformelement = document.querySelector("#search-form");
 searchformelement.addEventListener("submit", handlesearchsubmit);
